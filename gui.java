8b14fdb34098a7a37aaec56a0756ca4a1da8232a
RockyNg <48321546+RockyNg@users.noreply.github.com>
2019-04-08 19:39:57 +0100

    Add files via upload

--- gui.java
+++ gui.java
@@ -8,19 +8,52 @@ public class gui  extends JFrame
 {
 	private JPanel p1,p2,p3;
 	private JLabel label1,label2,label3;
-	private JRadioButton tempH,tempN,tempC,achesY,achesN,ThroatY,sThroatN;
+	private JRadioButton tempH,tempN,tempC,achesY,achesN,sThroatY,sThroatN;
 	private ButtonGroup group1,group2,group3;
+	private int a1,a2,a3,a4,a5,a6,a7;
 	
 	public gui()
 	{
 		super ("title");
 		setSize(300,200);
 		setLayout(new FlowLayout());
-		p1 = new JPanel();
-		p2 = new JPanel();
-		p3 = new JPanel();
-
 		
+		
+		
+		
+		tempH = new JRadioButton("Hot",false);
+		tempN = new JRadioButton("Normal",false);
+		tempC = new JRadioButton("Cool",false);
+		achesY = new JRadioButton("Yes",false);
+		achesN = new JRadioButton("No",false);
+		sThroatY = new JRadioButton("Yes",false);
+		sThroatN = new JRadioButton("No",false);
+		
+		add(tempH);
+		add(tempN);
+		add(tempC);
+		add(achesY);
+		add(achesN);
+		add(sThroatY);
+		add(sThroatN);
+		
+		group1 = group2 = group3 = new ButtonGroup();
+		
+		
+		group1.add(tempH);
+		group1.add(tempN);
+		group1.add(tempC);
+		
+		group2.add(achesY);
+		group2.add(achesN);
+		
+		group3.add(sThroatY);
+		group3.add(sThroatN);
+		
+		p1 = p2 = p3 = new JPanel(new FlowLayout());
+	
+
+	
 		p1.setBackground(Color.WHITE);
 		p2.setBackground(Color.GREEN);
 		p3.setBackground(Color.YELLOW);
@@ -30,46 +63,31 @@ public class gui  extends JFrame
 		add(p3,BorderLayout.SOUTH);
 
 		
-		
-
-		
 		label1 = new JLabel("Temperature",25);
 		label2 = new JLabel("Aches",25);
 		label3 = new JLabel("Sore Throat",25);
-		add(label1);
-		add(label2);
-		add(label3);
+		p1.add(label1);
+		p2.add(label2);
+		p3.add(label3);
+		setVisible(true);
+		
+		a1 = 1;
+		a2 = 2;
+		a3 = 3;
+		a4 = 4;
+		a5 = 5;
+		a6 = 6;
+		a7 = 7;
 		
 		//tempH,tempN,tempC,achesY,achesN,ThroatY,sThroatN;
-		tempH = new JRadioButton("Hot",false);
-		tempN = new JRadioButton("Normal",false);
-		tempC = new JRadioButton("Cool",false);
-		achesY = new JRadioButton("Yes",false);
-		achesN = new JRadioButton("No",false);
-		ThroatY = new JRadioButton("Yes",false);
-		sThroatN = new JRadioButton("No",false);
-		
-		add(pb);
-		add(bb);
-		add(ib);
-		add(bib);
-		
-		group = new ButtonGroup();
-		group.add(bb);
-		group.add(ib);
-		group.add(bib);
-		group.add(pb);
-		
-		
-		bf = new Font("Serif",Font.BOLD,14);
-		itf = new Font("Serif",Font.ITALIC,14);
-		bif = new Font("Serif",Font.BOLD + Font.ITALIC,14);
-		tf.setFont(pf);
-		
 		//wait for event to happen, pass in font object to constructor
-		bb.addItemListener(new HandlerClass(bf));
-		ib.addItemListener(new HandlerClass(itf));
-		bib.addItemListener(new HandlerClass(bif));
+		tempH.addItemListener(new HandlerClass(itf);
+		tempN.addItemListener(new HandlerClass(itf));
+		tempC.addItemListener(new HandlerClass(bif));
+		achesY.addItemListener(new HandlerClass(bif));
+		achesN.addItemListener(new HandlerClass(bif));
+		sThroatY.addItemListener(new HandlerClass(bif));
+		sThroatN.addItemListener(new HandlerClass(bif));
 		
 		
 		
