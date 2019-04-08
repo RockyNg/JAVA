a84c111785f31b7a58e4bfbe6d714a814d333651
RockyNg <48321546+RockyNg@users.noreply.github.com>
2019-04-08 19:16:04 +0100

    Add files via upload

--- gui.java
+++ gui.java
@@ -6,24 +6,50 @@ import javax.swing.*;
 
 public class gui  extends JFrame
 {
-	private JTextField tf;
-	private Font pf,bf,itf,bif;
-	private JRadioButton pb,bb,ib,bib;
-	private ButtonGroup group;
+	private JPanel p1,p2,p3;
+	private JLabel label1,label2,label3;
+	private JRadioButton tempH,tempN,tempC,achesY,achesN,ThroatY,sThroatN;
+	private ButtonGroup group1,group2,group3;
 	
 	public gui()
 	{
 		super ("title");
+		setSize(300,200);
 		setLayout(new FlowLayout());
-		tf = new JTextField("Why hello there",25);
-		add(tf);
-		
-		pb = new JRadioButton("Plain",false);
+		p1 = new JPanel();
+		p2 = new JPanel();
+		p3 = new JPanel();
 
-		bb = new JRadioButton("bold",false);
-		ib = new JRadioButton("italic",false);
-		bib = new JRadioButton("b and i",false);
 		
+		p1.setBackground(Color.WHITE);
+		p2.setBackground(Color.GREEN);
+		p3.setBackground(Color.YELLOW);
+		
+		add(p1,BorderLayout.CENTER);
+		add(p2,BorderLayout.NORTH);
+		add(p3,BorderLayout.SOUTH);
+
+		
+		
+
+		
+		label1 = new JLabel("Temperature",25);
+		label2 = new JLabel("Aches",25);
+		label3 = new JLabel("Sore Throat",25);
+		add(label1);
+		add(label2);
+		add(label3);
+		
+		//tempH,tempN,tempC,achesY,achesN,ThroatY,sThroatN;
+		tempH = new JRadioButton("Hot",false);
+		tempN = new JRadioButton("Normal",false);
+		tempC = new JRadioButton("Cool",false);
+		achesY = new JRadioButton("Yes",false);
+		achesN = new JRadioButton("No",false);
+		ThroatY = new JRadioButton("Yes",false);
+		sThroatN = new JRadioButton("No",false);
+		
+		add(pb);
 		add(bb);
 		add(ib);
 		add(bib);
@@ -33,6 +59,7 @@ public class gui  extends JFrame
 		group.add(ib);
 		group.add(bib);
 		group.add(pb);
+		
 		
 		bf = new Font("Serif",Font.BOLD,14);
 		itf = new Font("Serif",Font.ITALIC,14);
