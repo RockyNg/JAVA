6e5a0f26a114761a3fedc3624f0fa6191479039d
RockyNg <48321546+RockyNg@users.noreply.github.com>
2019-04-09 22:22:09 +0100

    Add files via upload

--- nbayes.java
+++ nbayes.java
@@ -3,7 +3,11 @@ package rocky;
 public class nbayes 
 {
 	temperature hey =new temperature();
+	aches hey2 =new aches();
+	soreThroat hey3 =new soreThroat();
 	
 	private double[] dest = ((hey.result).clone());
+	private double[] dest2 = ((hey2.result).clone());
+	private double[] dest3 = ((hey3.result).clone());
 
 }
