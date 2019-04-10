ead84782f1aac861a7ada4c5e20b22dc292b2cbb
RockyNg <48321546+RockyNg@users.noreply.github.com>
2019-04-10 23:36:51 +0100

    Add files via upload

--- gui.java
+++ gui.java
@@ -9,15 +9,16 @@ public class gui  extends JFrame implements ActionListener
 {
 	
 	//declare components needed for the gui
-	private JPanel p1,p2,p3;
+	private JPanel p1,p2,p3,p4;
 	private JLabel label1,label2,label3,label4;
 	private JRadioButton tempH,tempN,tempC,achesY,achesN,sThroatY,sThroatN;
 	private ButtonGroup group1,group2,group3;
-	private JButton b1;
+	private JButton b1,b2;
 	private Font big;
 	//declare variables to store user input
 	private double temp,aches,soreThroat,chance=0;
 	
+	private JList peoplelist;
 	private static String[] people= {"bacon","bacon2","bacon3","bacon4"};
 	//bring in calculations from nbayes class
 	nbayes finalCalculations =new nbayes();
@@ -97,13 +98,17 @@ public class gui  extends JFrame implements ActionListener
 	public gui()
 	{
 		
-		super ("title");
-		setSize(400,500);
+		super ("Tongsilltis prediction program");
+		setSize(500,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new FlowLayout());
 		
-		b1= new JButton("show");
+		b1= new JButton("Submit");
+		b2= new JButton("Another diagnosis");
 		
+		peoplelist= new JList(people);
+		peoplelist.setVisibleRowCount(3);
+		peoplelist.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		
 		tempH = new JRadioButton("Hot",false);
 		tempN = new JRadioButton("Normal",false);
@@ -140,17 +145,19 @@ public class gui  extends JFrame implements ActionListener
 		p1 = new JPanel();
 		p2 = new JPanel();
 		p3 = new JPanel();
+		p4 = new JPanel();
 
 		p1.setBackground(Color.WHITE);
 		p2.setBackground(Color.WHITE);
 		p3.setBackground(Color.WHITE);
+		p4.setBackground(Color.WHITE);
 
 
 		big = new Font("Serif", Font.ITALIC,20);
 		
-		label1 = new JLabel("Temperature");	
-		label2 = new JLabel("Aches");
-		label3 = new JLabel("Sore Throat");
+		label1 = new JLabel("What best describes your Temperature?");	
+		label2 = new JLabel("Do you have any Aches?");
+		label3 = new JLabel("Do you have a Sore Throat?");
 		label4 = new JLabel("Please enter your symptoms below:");
 		
 		label4.setFont(big);
@@ -158,6 +165,8 @@ public class gui  extends JFrame implements ActionListener
 		add(p1,BorderLayout.CENTER);
 		add(p2,BorderLayout.SOUTH);
 		add(p3,BorderLayout.SOUTH);
+		add(p4,BorderLayout.SOUTH);
+		add(new JScrollPane(peoplelist));
 		
 		p1.add(label1);
 		p1.add(tempH);
@@ -171,11 +180,14 @@ public class gui  extends JFrame implements ActionListener
 		p3.add(label3);
 		p3.add(sThroatY);
 		p3.add(sThroatN);
-		p3.add(b1);
+		p4.add(b1);
+		p4.add(b2);
 		
 		
 		setVisible(true);
 		b1.addActionListener(this);
+		b2.addActionListener(this);
+		
 
 		//wait for event to happen, whatever the user selects will enter the correct probability
 		  
@@ -215,9 +227,25 @@ public class gui  extends JFrame implements ActionListener
 				
 			    JOptionPane.showMessageDialog(this,(df.format(chance))+"% chance of having tongsillitis\n You don't have tongsillitis");
 			}
-			}
+			
 			  	   
 		     	
+		   } }
+		if (e.getSource() ==b2)
+		   {	
+			
+			
+			  
+			      temp=0;
+			        aches=0;
+			        soreThroat=0;
+			        chance=0;
+			        group1.clearSelection();
+			        group2.clearSelection();
+			        group3.clearSelection();
+
+
+			
 		   } 
 		
 	}
